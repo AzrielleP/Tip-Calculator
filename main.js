f6a78e70477646dc5cd519b2b5e4a8b5bcd7ea3b
AzrielleP <azrielle101@gmail.com>
2020-05-17 00:26:38 +0800

    Fixed some errors, formatted the files

--- main.js
+++ main.js
@@ -1,67 +1,63 @@
-const rateService = document.getElementById("rate-service");
-const calc = document.querySelector(".submit")
+const rateService = document.getElementById("rateService");
+const calc = document.querySelector(".submit");
 let output = document.querySelector(".calculatedResult");
 
 let total = 0;
 let rate = null;
 
-function calculate(){
-    const billAmount = Number(document.getElementById("bill-amount").value);
-    const personNumber = Number(document.getElementById("person-number").value);
+function calculate() {
+  const billAmount = Number(document.getElementById("billAmount").value);
+  const personNumber = Number(document.getElementById("personNumber").value);
 
-    if (billAmount == ""){
-        alert("Please enter your bill amount.");
-        return false;
-    }
+  if (billAmount == "") {
+    alert("Please enter your bill amount.");
+    return false;
+  }
 
-    if (personNumber == "" || personNumber == 0){
-        alert("Please enter a valid number of people.");
-        return false;
-    }
+  if (personNumber == "" || personNumber == 0) {
+    alert("Please enter a valid number of people.");
+    return false;
+  }
 
-    if (rateService.disabled == false){
-        serviceRate();
-        if (!serviceRate()) return false;
-        else total = (billAmount + billAmount*rate)/personNumber;
-    }
-    else{ 
-        
-        total = billAmount / personNumber;
-    }
-    
-    output.textContent = "$ " + total.toFixed(2);
-    //output.style.visibility = 'visible';
+  if (rateService.disabled == false) {
+    serviceRate();
+    if (!serviceRate()) return false;
+    else total = (billAmount + billAmount * rate) / personNumber;
+  } else {
+    total = billAmount / personNumber;
+  }
+
+  output.textContent = "$ " + total.toFixed(2);
 }
 
-calc.addEventListener('click', calculate);
+calc.addEventListener("click", calculate);
 
-
-function enableRate(){
-    rateService.disabled = enableTip.checked? false:true;
+function enableRate() {
+  rateService.disabled = enableTip.checked ? false : true;
 }
 
-function serviceRate(){
-    const choice = rateService.value;
+function serviceRate() {
+  const choice = rateService.value;
 
-    switch(choice){
-        case "":
-            alert("Please choose from the options.")
-            return false;
-        case "excellent":
-            rate = 0.3;
-            break;
-        case "good":
-            rate = 0.2;
-            break;
-        case "okay":
-            rate = 0.15;
-            break;
-        case "bad":
-            rate = 0.1;
-            break;
-        case "terrible":
-            rate = 0.05;
-            break;
-    }
-    return rate;
+  switch (choice) {
+    case "":
+      alert("Please choose from the options.");
+      return false;
+    case "excellent":
+      rate = 0.3;
+      break;
+    case "good":
+      rate = 0.2;
+      break;
+    case "okay":
+      rate = 0.15;
+      break;
+    case "bad":
+      rate = 0.1;
+      break;
+    case "terrible":
+      rate = 0.05;
+      break;
+  }
+  return rate;
 }
