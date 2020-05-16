24aad098cb8838f18ad6ff4776e7c389df7e86ad
AzrielleP <azrielle101@gmail.com>
2020-05-16 22:00:39 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const rateService = document.getElementById("rate-service");
+const calc = document.querySelector(".submit")
+let output = document.querySelector(".calculatedResult");
+
+let total = 0;
+let rate = null;
+
+function calculate(){
+    const billAmount = document.getElementById("bill-amount").value;
+    const personNumber = document.getElementById("person-number").value;
+    if (rateService.disabled == false){
+        serviceRate();
+        total = (billAmount + (billAmount*rate))/personNumber;
+
+    }
+    else{ 
+        
+        total = billAmount / personNumber;
+    }
+    
+    output.textContent = total;
+    output.style.visibility = 'visible';
+}
+
+calc.addEventListener('click', calculate);
+
+
+function enableRate(){
+    rateService.disabled = enableTip.checked? false:true;
+}
+function serviceRate(){
+    const choice = rateService.value;
+
+    switch(choice){
+        case "excellent ":
+            rate = 0.3;
+            break;
+        case "good":
+            rate = 0.2;
+            break;
+        case "okay":
+            rate = 0.15;
+            break;
+        case "bad":
+            rate = 0.1;
+            break;
+        case "terrible":
+            rate = 0.05;
+            break;
+    }
+    return rate;
+}
